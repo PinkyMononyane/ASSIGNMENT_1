da0dae783c22676797a9127b35616caaf8361837
PinkyMononyane <43929346@mylife.unisa.ac.za>
2017-08-17 16:18:31 +0100

    Add files via upload

---
+++ ClubMembers/ClubMembers/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Me.Hide()
+        SIGNUP.Show()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Me.Hide()
+        CLUBMEMBERS.Show()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+End Class
